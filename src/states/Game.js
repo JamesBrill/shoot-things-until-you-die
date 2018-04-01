00d549387f1f83c299587800960c8887dee9d7e3
James Brill <contact@jamesbrill.co.uk>
2018-04-01 20:15:07 +0100

    Movable camera

--- src/states/Game.js
+++ src/states/Game.js
@@ -3,33 +3,51 @@ import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
-  init() { }
-  preload() { }
+  init () {}
+  preload () {}
 
-  create() {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#77BFA3',
-      smoothed: false
-    })
+  create () {
+    this.game.add.tileSprite(0, 0, 1920, 1920, 'background')
 
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
+    this.game.world.setBounds(0, 0, 1920, 1920)
 
-    this.mushroom = new Mushroom({
+    this.game.physics.startSystem(Phaser.Physics.P2JS)
+
+    this.player = new Mushroom({
       game: this.game,
       x: this.world.centerX,
       y: this.world.centerY,
       asset: 'mushroom'
     })
+    this.game.add.existing(this.player)
+    this.game.physics.p2.enable(this.player)
 
-    this.game.add.existing(this.mushroom)
+    this.player.body.fixedRotation = true
+
+    this.cursors = this.game.input.keyboard.createCursorKeys()
+
+    //  Notice that the sprite doesn't have any momentum at all,
+    //  it's all just set by the camera follow type.
+    //  0.1 is the amount of linear interpolation to use.
+    //  The smaller the value, the smooth the camera (and the longer it takes to catch up)
+    this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1)
   }
 
-  render() {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+  update () {
+    this.player.body.setZeroVelocity()
+
+    if (this.cursors.up.isDown) {
+      this.player.body.moveUp(300)
+    } else if (this.cursors.down.isDown) {
+      this.player.body.moveDown(300)
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -300
+    } else if (this.cursors.right.isDown) {
+      this.player.body.moveRight(300)
     }
   }
+
+  render () {}
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -5,8 +5,16 @@ export default class extends Phaser.State {
   init () {}
 
   preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
+    this.loaderBg = this.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'loaderBg'
+    )
+    this.loaderBar = this.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'loaderBar'
+    )
     centerGameObjects([this.loaderBg, this.loaderBar])
 
     this.load.setPreloadSprite(this.loaderBar)

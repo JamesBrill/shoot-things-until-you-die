c4ae34a564991506dfb19c63002f1e4bae325960
James Brill <contact@jamesbrill.co.uk>
2018-04-08 16:47:07 +0100

    Add second aimline

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -5,21 +5,32 @@ export default class extends Phaser.Sprite {
   constructor ({ game, x, y }) {
     const PLAYER_SIZE = 30
     const GUN_RANGE = 150
+    const GUN_RADIUS = 60
 
     const playerGraphics = game.add.graphics(x, y)
     playerGraphics.beginFill(0xff0000, 1)
     playerGraphics.drawCircle(x, y, PLAYER_SIZE)
     playerGraphics.endFill()
     super(game, x, y, playerGraphics.generateTexture())
-    this.NINETY_DEGREES_IN_RADIANS = game.math.degToRad(90)
 
+    this.gunRadiusRadians = game.math.degToRad(GUN_RADIUS)
+    this.NINETY_DEGREES_AS_RADIANS = game.math.degToRad(90)
     this.anchor.setTo(0.5)
     const aimLine = game.add.graphics(x, y)
     aimLine.lineStyle(1, 0xff0000)
     aimLine.moveTo(x, y)
     aimLine.lineTo(x, y - GUN_RANGE)
+    aimLine.moveTo(x, y)
+    const rotatedAimLinePoint = Phaser.Point.rotate(
+      new Phaser.Point(x, y - GUN_RANGE),
+      x,
+      y,
+      this.gunRadiusRadians
+    )
+    aimLine.lineTo(rotatedAimLinePoint.x, rotatedAimLinePoint.y)
     aimLine.endFill()
     this.aimLine = new Phaser.Sprite(game, 0, 0, aimLine.generateTexture())
+    this.aimLine.anchor.setTo(0, 1)
     this.addChild(this.aimLine)
     playerGraphics.destroy()
     aimLine.destroy()
@@ -28,7 +39,8 @@ export default class extends Phaser.Sprite {
   aimAt (x, y) {
     const { angleBetween } = this.game.math
     const aimAngle = angleBetween(this.world.x, this.world.y, x, y)
-    this.aimLine.rotation = aimAngle - this.NINETY_DEGREES_IN_RADIANS
+    this.aimLine.rotation =
+      aimAngle + (this.NINETY_DEGREES_AS_RADIANS - 0.5 * this.gunRadiusRadians)
   }
 
   move (direction, distance) {

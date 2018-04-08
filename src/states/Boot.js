cad6016f016ccf69ce88ddd140acbe3d54f5656b
James Brill <contact@jamesbrill.co.uk>
2018-04-08 17:53:37 +0100

    Lint

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,15 +1,15 @@
 import Phaser from 'phaser'
 import WebFont from 'webfontloader'
-import config from '../config';
+import config from '../config'
 
 export default class extends Phaser.State {
-  init() {
+  init () {
     this.stage.backgroundColor = '#EDEEC9'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
   }
 
-  preload() {
+  preload () {
     if (config.webfonts.length) {
       WebFont.load({
         google: {
@@ -19,14 +19,19 @@ export default class extends Phaser.State {
       })
     }
 
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
+    let text = this.add.text(
+      this.world.centerX,
+      this.world.centerY,
+      'loading fonts',
+      { font: '16px Arial', fill: '#dddddd', align: 'center' }
+    )
     text.anchor.setTo(0.5, 0.5)
 
     this.load.image('loaderBg', './assets/images/loader-bg.png')
     this.load.image('loaderBar', './assets/images/loader-bar.png')
   }
 
-  render() {
+  render () {
     if (config.webfonts.length && this.fontsReady) {
       this.state.start('Splash')
     }
@@ -35,7 +40,7 @@ export default class extends Phaser.State {
     }
   }
 
-  fontsLoaded() {
+  fontsLoaded () {
     this.fontsReady = true
   }
 }

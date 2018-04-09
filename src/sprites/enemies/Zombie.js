1117479d7b286ffeb21ff9947a7f8d40a8de3062
James Brill <contact@jamesbrill.co.uk>
2018-04-09 23:03:39 +0100

    Zombies can spawn anywhere in the world

--- src/sprites/enemies/Zombie.js
+++ src/sprites/enemies/Zombie.js
@@ -20,8 +20,9 @@ export default class Zombie extends Phaser.Sprite {
 }
 
 Zombie.createRandom = (game, player, world, maxSpeed) => {
-  const randomX = Math.random() * 10000 - 5000
-  const randomY = Math.random() * 10000 - 5000
+  const { width, height } = world
+  const randomX = Math.random() * width - 0.5 * width
+  const randomY = Math.random() * height - 0.5 * height
   const randomSpeed = Math.random() * (maxSpeed || 200) + 30
   return new Zombie({
     game,

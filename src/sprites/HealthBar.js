16122c7feea521da1707f9abfa8a03951fc45113
James Brill <contact@jamesbrill.co.uk>
2018-04-10 21:54:31 +0100

    Create 3 categories of zombie, each with different size, speed and health

--- src/sprites/HealthBar.js
+++ src/sprites/HealthBar.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
 
 export default class HealthBar extends Phaser.Sprite {
-  constructor ({ game, x, y }) {
+  constructor ({ game, x, y, maxHealth }) {
     const healthBar = game.add.graphics(0, 0)
     healthBar.lineStyle(1, 0x000000)
     healthBar.lineTo(0, HealthBar.HEIGHT)
@@ -10,14 +10,15 @@ export default class HealthBar extends Phaser.Sprite {
     healthBar.lineTo(0, 0)
     super(game, x, y, healthBar.generateTexture())
     this.game = game
+    this.maxHealth = maxHealth || 100
     healthBar.destroy()
-    this.setHealth(100)
+    this.setHealth(this.maxHealth)
   }
 
   getHealthBarColour (health) {
-    if (health > 66) {
+    if (health > 2 / 3 * this.maxHealth) {
       return 0x00ff00
-    } else if (health > 33) {
+    } else if (health > 1 / 3 * this.maxHealth) {
       return 0xffff00
     } else {
       return 0xff0000
@@ -26,7 +27,7 @@ export default class HealthBar extends Phaser.Sprite {
 
   setHealth (health) {
     const healthBarColour = this.getHealthBarColour(health)
-    const healthContentsWidth = HealthBar.WIDTH * (health / 100)
+    const healthContentsWidth = HealthBar.WIDTH * (health / this.maxHealth)
     this.removeChild(this.healthBarContents)
     const healthBarContents = this.game.add.graphics(0, 0)
     healthBarContents.beginFill(healthBarColour)
--- src/sprites/enemies/Zombie.js
+++ src/sprites/enemies/Zombie.js
@@ -2,23 +2,26 @@ import Phaser from 'phaser'
 import HealthBar from '../HealthBar'
 
 export default class Zombie extends Phaser.Sprite {
-  constructor ({ game, x, y, player, speed }) {
+  constructor ({ game, x, y, player, speed, size }) {
     const enemyGraphics = game.add.graphics(x, y)
     enemyGraphics.beginFill(0x00ff00, 1)
-    enemyGraphics.drawCircle(x, y, Zombie.ENEMY_SIZE)
+    enemyGraphics.drawCircle(x, y, size)
     enemyGraphics.endFill()
     super(game, x, y, enemyGraphics.generateTexture())
     enemyGraphics.destroy()
     this.anchor.setTo(0.5)
     this.game = game
     this.player = player
-    this.speed = speed
+    this.sizeModifier = size / Zombie.NORMAL_SIZE
+    this.speed = speed / this.sizeModifier
     this.attackDamage = 3
-    this.health = 100
+    this.maxHealth = 100 * this.sizeModifier
+    this.health = this.maxHealth
     this.healthBar = new HealthBar({
       game,
       x: -0.5 * HealthBar.WIDTH,
-      y: -Zombie.ENEMY_SIZE
+      y: -size,
+      maxHealth: this.maxHealth
     })
     this.addChild(this.healthBar)
   }
@@ -43,13 +46,25 @@ Zombie.createRandom = (game, player, world, maxSpeed) => {
   const randomX = Math.random() * width - 0.5 * width
   const randomY = Math.random() * height - 0.5 * height
   const randomSpeed = Math.random() * (maxSpeed || 200) + 30
+  const zombieSizePicker = Math.random()
+  let zombieSize
+  if (zombieSizePicker < 0.1) {
+    zombieSize = Zombie.XLARGE_SIZE
+  } else if (zombieSizePicker < 0.8) {
+    zombieSize = Zombie.LARGE_SIZE
+  } else {
+    zombieSize = Zombie.NORMAL_SIZE
+  }
   return new Zombie({
     game,
     x: world.centerX + randomX,
     y: world.centerY + randomY,
     player,
-    speed: randomSpeed
+    speed: randomSpeed,
+    size: zombieSize
   })
 }
 
-Zombie.ENEMY_SIZE = 30
+Zombie.NORMAL_SIZE = 30
+Zombie.LARGE_SIZE = 60
+Zombie.XLARGE_SIZE = 90

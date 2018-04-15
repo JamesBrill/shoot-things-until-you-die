924e7f7ea08b6fba0177753b3393d5ec39075b26
James Brill <contact@jamesbrill.co.uk>
2018-04-15 16:11:38 +0100

    Have zombie respawn distance decrease over time

--- src/ai/Director.js
+++ src/ai/Director.js
@@ -8,6 +8,15 @@ export default class Director {
     this.enemies = enemies
     this.maxZombieSpeed = 200
     this.healthMultiplier = 1
+    this.intensity = 0
+    setTimeout(this.increaseIntensity.bind(this), 3000)
+  }
+
+  increaseIntensity () {
+    this.intensity += 1
+    if (this.intensity < 100) {
+      setTimeout(this.increaseIntensity.bind(this), 3000)
+    }
   }
 
   initialiseZombies () {
@@ -16,7 +25,7 @@ export default class Director {
       const randomPosition = getPositionAtDistanceRangeFromPlayer(
         this.game.world,
         this.player,
-        1000,
+        1500,
         width * 0.45
       )
       this.enemies.add(
@@ -38,8 +47,8 @@ export default class Director {
     const randomPosition = getPositionAtDistanceRangeFromPlayer(
       this.game.world,
       this.player,
-      600,
-      800
+      this.getMinEnemyDistanceFromPlayer(),
+      this.getMaxEnemyDistanceFromPlayer()
     )
     this.enemies.add(
       Zombie.createRandom(
@@ -51,5 +60,13 @@ export default class Director {
         this.healthMultiplier
       )
     )
+  }
+
+  getMinEnemyDistanceFromPlayer () {
+    return 800 + (100 - this.intensity) * 15
+  }
+
+  getMaxEnemyDistanceFromPlayer () {
+    return this.getMinEnemyDistanceFromPlayer() + (101 - this.intensity) * 30
   }
 }

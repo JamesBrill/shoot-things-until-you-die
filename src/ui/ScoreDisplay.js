e7797aed6979c035816c03220d332def0de5bd90
James Brill <contact@jamesbrill.co.uk>
2018-04-17 21:57:03 +0100

    Centre score text properly

--- src/ui/ScoreDisplay.js
+++ src/ui/ScoreDisplay.js
@@ -1,20 +1,13 @@
 export default class ScoreDisplay {
   constructor ({ game, score, multiplier }) {
     this.game = game
-    this.scoreText = this.game.add.text(
-      this.game.width / 2,
-      8,
-      `${score} (x${multiplier})`,
-      {
-        font: '32px Arial',
-        fill: 'red',
-        align: 'center',
-        boundsAlignH: 'center',
-        boundsAlignV: 'middle'
-      }
-    )
+    this.scoreText = this.game.add.text(0, 0, `${score} (x${multiplier})`, {
+      font: '32px Arial',
+      fill: 'red',
+      boundsAlignH: 'center'
+    })
     this.scoreText.fixedToCamera = true
-    this.scoreText.cameraOffset.setTo(this.game.width / 2, 8)
+    this.scoreText.setTextBounds(0, 0, this.game.width, this.game.height)
   }
 
   setScore (score, multiplier) {

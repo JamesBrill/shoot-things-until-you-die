65118262194b63f738add9d21556462927c9e2ea
James Brill <contact@jamesbrill.co.uk>
2019-12-14 19:53:23 +0000

    Fix blood drip positioning

--- src/sprites/BloodDrip.js
+++ src/sprites/BloodDrip.js
@@ -2,18 +2,20 @@ import Phaser from 'phaser'
 
 export default class BloodDrip extends Phaser.Sprite {
   constructor ({ game, player }) {
-    const size = Math.max(Math.random(), 0.3) * player.size
-    const x = player.world.x - 0.5 * player.size
-    const y = player.world.y - 0.5 * player.size
     const random = Math.floor(Math.random() * 3)
-    let texture
+    let texture, size
     if (random === 0) {
       texture = BloodDrip.getBigTexture(game, player.size)
+      size = player.size * BloodDrip.BIG_MULTIPLIER
     } else if (random === 1) {
       texture = BloodDrip.getMediumTexture(game, player.size)
+      size = player.size * BloodDrip.MEDIUM_MULTIPLIER
     } else {
       texture = BloodDrip.getSmallTexture(game, player.size)
+      size = player.size * BloodDrip.SMALL_MULTIPLIER
     }
+    const x = player.world.x - 0.5 * size
+    const y = player.world.y - 0.5 * size
     super(game, x, y, texture)
     const xAnchor = 0.5 * size / this.width
     const yAnchor = 0.5 * size / this.height
@@ -40,21 +42,21 @@ BloodDrip.createTexture = (game, size) => {
 
 BloodDrip.getBigTexture = (game, playerSize) => {
   if (!BloodDrip.BIG_TEXTURE) {
-    BloodDrip.BIG_TEXTURE = BloodDrip.createTexture(game, playerSize * 0.9)
+    BloodDrip.BIG_TEXTURE = BloodDrip.createTexture(game, playerSize * BloodDrip.BIG_MULTIPLIER)
   }
   return BloodDrip.BIG_TEXTURE
 }
 
 BloodDrip.getMediumTexture = (game, playerSize) => {
   if (!BloodDrip.MEDIUM_TEXTURE) {
-    BloodDrip.MEDIUM_TEXTURE = BloodDrip.createTexture(game, playerSize * 0.7)
+    BloodDrip.MEDIUM_TEXTURE = BloodDrip.createTexture(game, playerSize * BloodDrip.MEDIUM_MULTIPLIER)
   }
   return BloodDrip.MEDIUM_TEXTURE
 }
 
 BloodDrip.getSmallTexture = (game, playerSize) => {
   if (!BloodDrip.SMALL_TEXTURE) {
-    BloodDrip.SMALL_TEXTURE = BloodDrip.createTexture(game, playerSize * 0.5)
+    BloodDrip.SMALL_TEXTURE = BloodDrip.createTexture(game, playerSize * BloodDrip.SMALL_MULTIPLIER)
   }
   return BloodDrip.SMALL_TEXTURE
 }
@@ -62,3 +64,6 @@ BloodDrip.getSmallTexture = (game, playerSize) => {
 BloodDrip.BIG_TEXTURE = null
 BloodDrip.MEDIUM_TEXTURE = null
 BloodDrip.SMALL_TEXTURE = null
+BloodDrip.BIG_MULTIPLIER = 0.9
+BloodDrip.MEDIUM_MULTIPLIER = 0.7
+BloodDrip.SMALL_MULTIPLIER = 0.5

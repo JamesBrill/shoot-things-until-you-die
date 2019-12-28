346b928caca10270d8bfb35b6fdfdf3e6800ea48
James Brill <contact@jamesbrill.co.uk>
2019-12-28 23:11:03 +0000

    Add P90 sprite

--- src/sprites/ammoDrops/AmmoDrop.js
+++ src/sprites/ammoDrops/AmmoDrop.js
@@ -10,34 +10,28 @@ export default class AmmoDrop extends Phaser.Sprite {
 
 AmmoDrop.createRandom = (game, x, y) => {
   const ammoTypePicker = Math.random()
-  let ammoType, weaponName, bullets, spriteName
+  let weaponName, bullets, spriteName
   if (ammoTypePicker < 0.16) {
-    ammoType = AmmoDrop.PISTOL_AMMO_SPRITE
     weaponName = 'pistol'
     bullets = 64
     spriteName = 'pistol_ammo'
   } else if (ammoTypePicker < 0.32) {
-    ammoType = AmmoDrop.LEVER_ACTION_SHOTGUN_AMMO_SPRITE
     weaponName = 'leverActionShotgun'
     bullets = 24
     spriteName = 'model_1887_ammo'
   } else if (ammoTypePicker < 0.48) {
-    ammoType = AmmoDrop.SEMI_AUTO_SHOTGUN_AMMO_SPRITE
     weaponName = 'semiAutoShotgun'
     bullets = 30
     spriteName = 'shotgun_ammo'
   } else if (ammoTypePicker < 0.64) {
-    ammoType = AmmoDrop.ASSAULT_RIFLE_AMMO_SPRITE
     weaponName = 'p90'
     bullets = 200
-    spriteName = 'assault_rifle_ammo'
+    spriteName = 'p90'
   } else if (ammoTypePicker < 0.8) {
-    ammoType = AmmoDrop.GHOST_PISTOL_AMMO_SPRITE
     weaponName = 'ghostPistol'
     bullets = 64
     spriteName = 'ghost_pistol_ammo'
   } else {
-    ammoType = AmmoDrop.ASSAULT_RIFLE_AMMO_SPRITE
     weaponName = 'm16'
     bullets = 60
     spriteName = 'assault_rifle_ammo'
@@ -46,7 +40,6 @@ AmmoDrop.createRandom = (game, x, y) => {
     game,
     x,
     y,
-    ammoType,
     weaponName,
     bullets,
     spriteName
@@ -56,9 +49,3 @@ AmmoDrop.createRandom = (game, x, y) => {
 AmmoDrop.addAmmo = (weapons, ammoDrop) => {
   weapons[ammoDrop.weaponName].pickUpAmmo(ammoDrop.bullets)
 }
-
-AmmoDrop.PISTOL_AMMO_SPRITE = 10
-AmmoDrop.LEVER_ACTION_SHOTGUN_AMMO_SPRITE = 15
-AmmoDrop.SEMI_AUTO_SHOTGUN_AMMO_SPRITE = 20
-AmmoDrop.ASSAULT_RIFLE_AMMO_SPRITE = 25
-AmmoDrop.GHOST_PISTOL_AMMO_SPRITE = 25
--- src/states/Boot.js
+++ src/states/Boot.js
@@ -79,6 +79,7 @@ export default class extends Phaser.State {
     this.load.image('shotgun_ammo', 'assets/images/shotgun_ammo.png')
     this.load.image('health_pack', 'assets/images/health_pack.png')
     this.load.image('ghost_pistol_ammo', 'assets/images/ghost_pistol_ammo.png')
+    this.load.image('p90', 'assets/images/p90_large.png')
 
     // LOAD MAP
     this.load.tilemap('map', 'assets/maps/shootmap.csv', null, Phaser.Tilemap.CSV)

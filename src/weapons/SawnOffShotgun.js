6bc9690188a826693d3034e8efe6aa2e21c603ee
James Brill <contact@jamesbrill.co.uk>
2020-01-01 21:56:23 +0000

    Sawn-off shotgun tweaks

--- src/weapons/SawnOffShotgun.js
+++ src/weapons/SawnOffShotgun.js
@@ -19,6 +19,38 @@ export default class SawnOffShotgun extends Shotgun {
       displayName: SawnOffShotgun.DISPLAY_NAME,
       displayY: SawnOffShotgun.DISPLAY_Y
     })
+
+    this.loadTime = SawnOffShotgun.LOAD_TIME
+  }
+
+  reload () {
+    if (
+      this.ammoReserves > 0 &&
+      !this.reloading &&
+      this.currentAmmo !== this.maxBullets
+    ) {
+      const bulletsToLoad = Math.min(
+        this.ammoReserves,
+        this.maxBullets - this.currentAmmo
+      )
+      const callback = () => {
+        this.ammoReserves -= bulletsToLoad
+        this.currentAmmo += bulletsToLoad
+        this.weaponDisplay.setAmmoReserves(this.ammoReserves)
+        this.weaponDisplay.setCurrentAmmo(this.currentAmmo)
+        this.reloading = false
+      }
+      this.reloading = true
+      this.loadSound.play()
+      if (this.currentAmmo === 0 && this.ammoReserves > 1) {
+        setTimeout(() => this.loadSound.play(), this.loadTime)
+        setTimeout(() => this.cockSound.play(), 2 * this.loadTime)
+        this.reloadTimeout = setTimeout(callback.bind(this), 2 * this.loadTime + this.reloadTime)
+      } else {
+        setTimeout(() => this.cockSound.play(), this.loadTime)
+        this.reloadTimeout = setTimeout(callback.bind(this), this.loadTime + this.reloadTime)
+      }
+    }
   }
 
   hitTarget () {
@@ -27,13 +59,14 @@ export default class SawnOffShotgun extends Shotgun {
   }
 }
 
-SawnOffShotgun.GUN_RANGE = 250
+SawnOffShotgun.GUN_RANGE = 200
 SawnOffShotgun.GUN_ANGLE = 45
 SawnOffShotgun.NUMBER_OF_BULLETS = 12
-SawnOffShotgun.FIRE_RATE = 300
+SawnOffShotgun.FIRE_RATE = 0
 SawnOffShotgun.BULLET_SPEED = 1000
 SawnOffShotgun.ATTACK_DAMAGE = 300
-SawnOffShotgun.RELOAD_TIME = 400
+SawnOffShotgun.LOAD_TIME = 400
+SawnOffShotgun.RELOAD_TIME = 1410
 SawnOffShotgun.MAX_BULLETS = 2
 SawnOffShotgun.AMMO_RESERVES = 30
 SawnOffShotgun.DISPLAY_NAME = 'Sawn-off Shotgun'
